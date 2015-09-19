447b1a204224f559bcfa5aafce525e98bbbc82aa
Alan Smith <a.bax.smith@gmail.com>
2015-09-19 17:05:24 -0600

    adds router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,5 @@
 import React from 'react/addons';
-import Router from 'react-router';
+import { Router, Route, Link } from 'react-router';
 
 let Hello = React.createClass({
   render() {
@@ -7,4 +7,42 @@ let Hello = React.createClass({
   }
 });
 
-React.render(<Hello name='World'/>, document.body);
+let App = React.createClass({
+  render() {
+    return(
+      <div>
+        <Hello name='Router'/>
+        {this.props.children}
+      </div>
+    )
+  }
+});
+
+let Pizzerias = React.createClass({
+  render() {
+    return(
+      <h3>All the pizzerias!</h3>
+    )
+  }
+});
+
+let About = React.createClass({
+  render() {
+    return(
+      <div>
+        <h1>About</h1>
+        <p>This is a client application for the pizza repo!</p>
+      </div>
+    )
+  }
+});
+
+React.render((
+  <Router>
+    <Route path="/" component={App}>
+      <Route path='/pizzerias' component={Pizzerias}/>
+      <Route path="about" component={About}/>
+    </Route>
+  </Router>
+), document.body);
+
