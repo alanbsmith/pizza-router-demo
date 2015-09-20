6995cd13777ad8c3b43a2069e0802e8ad7c34a28
Alan Smith <a.bax.smith@gmail.com>
2015-09-19 19:03:26 -0600

    adding a navbar

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,7 +3,28 @@ import { Router, Route, Link } from 'react-router';
 
 let Hello = React.createClass({
   render() {
-    return(<h1>Hello, {this.props.name}!</h1>)
+    return(
+      <nav className="navbar navbar-inverse">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand">Pizza Client</a>
+          </div>
+          <div id="navbar" className="navbar-collapse collapse">
+            <ul className="nav navbar-nav">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="about">About</Link></li>
+              <li><Link to="pizzerias">Pizzerias</Link></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
   }
 });
 
