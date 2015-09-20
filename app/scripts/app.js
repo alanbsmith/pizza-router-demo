b2e3a3bc47cbac656c7f51a36418c3457cb44037
Alan Smith <a.bax.smith@gmail.com>
2015-09-20 08:01:07 -0600

    returning a single location

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -60,7 +60,7 @@ let Pizzerias = React.createClass({
       return(
           <div className='thumbnail' key={index}>
             <div className='caption'>
-              <h5>{location.pizzeria}</h5>
+              <h5><Link to={'/pizzerias/' + (index + 1) }>{location.pizzeria}</Link></h5>
               <a href={"https://www.google.com/maps/place/" + location.address + ' ' + location.city} target='_blank'>{location.address}</a>
               <p>{location.city}</p>
               <a href={location.website} target='_blank'>Website</a>
@@ -74,6 +74,44 @@ let Pizzerias = React.createClass({
           {pizzerias}
       </div>
     )
+  }
+});
+
+let Pizzeria = React.createClass({
+  getInitialState() {
+    return({location: ''})
+  },
+  componentDidMount() {
+    $.ajax({
+      url: 'http://localhost:9292/api/v1/pizzerias/' + this.props.params.id,
+      type: 'get',
+      dataType: 'JSON',
+      success: function(data) {
+        this.setState({ location: data})
+      }.bind(this)
+    })
+  },
+
+  render() {
+    if(this.state.location.properties !== undefined){
+      let location = this.state.location.properties
+      
+      return(
+          <div>
+          <Link to='/pizzerias' className='btn btn-lg btn-danger' id='btn-back'>Back to list</Link>
+          <div className='thumbnail'>
+            <div className='caption'>
+              <h5>{location.pizzeria}</h5>
+              <a href={"https://www.google.com/maps/place/" + location.address + ' ' + location.city} target='_blank'>{location.address}</a>
+              <p>{location.city}</p>
+              <a href={location.website} target='_blank'>Website</a>
+            </div>
+          </div>
+          </div>
+      )
+    } else {
+      return(false);
+    }
   }
 });
 
@@ -92,6 +130,7 @@ React.render((
   <Router>
     <Route path="/" component={App}>
       <Route path='/pizzerias' component={Pizzerias}/>
+      <Route path='/pizzerias/:id' component={Pizzeria}/>
       <Route path="about" component={About}/>
     </Route>
   </Router>
